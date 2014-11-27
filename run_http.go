7e4b43733b7f5467d361ff3977078666028f1576
Odin <odinmanlee@gmail.com>
2014-11-27 13:05:09 +0800

    bugfix

--- run_http.go
+++ run_http.go
@@ -6,13 +6,13 @@ package ogo
  */
 import (
 	"fmt"
-	//"net/http"
+	"net/http"
 	"os"
 	"path/filepath"
 	"runtime"
 
-	//"github.com/Odinman/ogo/bind"
-	//"github.com/Odinman/ogo/graceful"
+	"github.com/Odinman/ogo/bind"
+	"github.com/Odinman/ogo/graceful"
 	"github.com/VividCortex/godaemon"
 	"github.com/nightlyone/lockfile"
 	"github.com/zenazn/goji"
@@ -77,22 +77,22 @@ func Run() {
 	goji.Serve()
 
 	// socket listen
-	//bind.WithFlag()
-	//listener := bind.Default()
-	//Debugger.Warn("Starting Ogo on: %s", listener.Addr().String())
+	bind.WithFlag()
+	listener := bind.Default()
+	Debugger.Warn("Starting Ogo on: %s", listener.Addr().String())
 
-	//graceful.HandleSignals()
-	//bind.Ready()
-	//graceful.PreHook(func() { WriteMsg("Received signal, gracefully stopping") })
-	//graceful.PostHook(func() { WriteMsg("Stopped") })
+	graceful.HandleSignals()
+	bind.Ready()
+	graceful.PreHook(func() { WriteMsg("Received signal, gracefully stopping") })
+	graceful.PostHook(func() { WriteMsg("Stopped") })
 
-	//err := graceful.Serve(listener, http.DefaultServeMux)
+	err := graceful.Serve(listener, http.DefaultServeMux)
 
-	//if err != nil {
-	//	Debugger.Critical(err.Error())
-	//}
+	if err != nil {
+		Debugger.Critical(err.Error())
+	}
 
-	//graceful.Wait()
+	graceful.Wait()
 }
 
 /* }}} */
