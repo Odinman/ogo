8465319409d66ee3110ff094ee19eba269077936
Odin <odinmanlee@gmail.com>
2015-01-31 15:53:52 +0800

    upate

--- utils/rand.go
+++ utils/rand.go
@@ -1,20 +1,21 @@
 package utils
 
 import (
-    "crypto/rand"
+	"crypto/rand"
 )
 
 // RandomCreateBytes generate random []byte by specify chars.
-func RandomCreateBytes(n int, alphabets ...byte) []byte {
-    const alphanum = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-    var bytes = make([]byte, n)
-    rand.Read(bytes)
-    for i, b := range bytes {
-        if len(alphabets) == 0 {
-            bytes[i] = alphanum[b%byte(len(alphanum))]
-        } else {
-            bytes[i] = alphabets[b%byte(len(alphabets))]
-        }
-    }
-    return bytes
+//func RandomCreateBytes(n int, alphabets ...byte) []byte {
+func RandomCreateBytes(n int, alphabets string) []byte {
+	const alphanum = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+	var bytes = make([]byte, n)
+	rand.Read(bytes)
+	for i, b := range bytes {
+		if len(alphabets) == 0 {
+			bytes[i] = alphanum[b%byte(len(alphanum))]
+		} else {
+			bytes[i] = alphabets[b%byte(len(alphabets))]
+		}
+	}
+	return bytes
 }
