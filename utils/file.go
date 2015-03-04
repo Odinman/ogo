65e2155041fc81d7358da80e827dec234123e80b
Odin <odinmanlee@gmail.com>
2015-03-04 17:56:04 +0800

    update

--- utils/file.go
+++ utils/file.go
@@ -1,87 +1,120 @@
 package utils
 
 import (
-    "bufio"
-    "errors"
-    "io"
-    "os"
-    "path/filepath"
-    "regexp"
+	"bufio"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"regexp"
 )
 
 // SelfPath gets compiled executable file absolute path
 func SelfPath() string {
-    path, _ := filepath.Abs(os.Args[0])
-    return path
+	path, _ := filepath.Abs(os.Args[0])
+	return path
 }
 
 // SelfDir gets compiled executable file directory
 func SelfDir() string {
-    return filepath.Dir(SelfPath())
+	return filepath.Dir(SelfPath())
 }
 
 // FileExists reports whether the named file or directory exists.
 func FileExists(name string) bool {
-    if _, err := os.Stat(name); err != nil {
-        if os.IsNotExist(err) {
-            return false
-        }
-    }
-    return true
+	if _, err := os.Stat(name); err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+	}
+	return true
 }
 
 // Search a file in paths.
 // this is often used in search config file in /etc ~/
 func SearchFile(filename string, paths ...string) (fullpath string, err error) {
-    for _, path := range paths {
-        if fullpath = filepath.Join(path, filename); FileExists(fullpath) {
-            return
-        }
-    }
-    err = errors.New(fullpath + " not found in paths")
-    return
+	for _, path := range paths {
+		if fullpath = filepath.Join(path, filename); FileExists(fullpath) {
+			return
+		}
+	}
+	err = errors.New(fullpath + " not found in paths")
+	return
 }
 
 // like command grep -E
 // for example: GrepFile(`^hello`, "hello.txt")
 // \n is striped while read
 func GrepFile(patten string, filename string) (lines []string, err error) {
-    re, err := regexp.Compile(patten)
-    if err != nil {
-        return
-    }
+	re, err := regexp.Compile(patten)
+	if err != nil {
+		return
+	}
 
-    fd, err := os.Open(filename)
-    if err != nil {
-        return
-    }
-    lines = make([]string, 0)
-    reader := bufio.NewReader(fd)
-    prefix := ""
-    isLongLine := false
-    for {
-        byteLine, isPrefix, er := reader.ReadLine()
-        if er != nil && er != io.EOF {
-            return nil, er
-        }
-        if er == io.EOF {
-            break
-        }
-        line := string(byteLine)
-        if isPrefix {
-            prefix += line
-            continue
-        } else {
-            isLongLine = true
-        }
+	fd, err := os.Open(filename)
+	if err != nil {
+		return
+	}
+	lines = make([]string, 0)
+	reader := bufio.NewReader(fd)
+	prefix := ""
+	isLongLine := false
+	for {
+		byteLine, isPrefix, er := reader.ReadLine()
+		if er != nil && er != io.EOF {
+			return nil, er
+		}
+		if er == io.EOF {
+			break
+		}
+		line := string(byteLine)
+		if isPrefix {
+			prefix += line
+			continue
+		} else {
+			isLongLine = true
+		}
 
-        line = prefix + line
-        if isLongLine {
-            prefix = ""
-        }
-        if re.MatchString(line) {
-            lines = append(lines, line)
-        }
-    }
-    return lines, nil
+		line = prefix + line
+		if isLongLine {
+			prefix = ""
+		}
+		if re.MatchString(line) {
+			lines = append(lines, line)
+		}
+	}
+	return lines, nil
+}
+
+// readLines reads a whole file into memory
+// and returns a slice of its lines.
+func ReadLines(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
+}
+
+// writeLines writes the lines to the given file.
+func WriteLines(lines []string, path string) error {
+	file, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	w := bufio.NewWriter(file)
+	for _, line := range lines {
+		fmt.Fprintln(w, line)
+	}
+	return w.Flush()
 }
