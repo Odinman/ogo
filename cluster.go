c2df38a2838ab17676060cc83ad87775df5aa732
Lijing <lijing@qinhutouzi.com>
2015-06-24 15:38:24 +0800

    add lock/release function(cluster)

--- cluster.go
+++ cluster.go
@@ -4,8 +4,11 @@ package ogo
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 	"time"
 
+	"github.com/Odinman/ogo/utils"
 	//"gopkg.in/redis.v3"
 )
 
@@ -35,16 +38,53 @@ func CacheGet(key string) (string, error) {
 
 /* }}} */
 
-/* {{{ func GetLock(tag string) string
+/* {{{ func GetLock(key string) string
  * 获取锁
  */
 func GetLock(key string) string {
 	if cc := ClusterClient(); cc != nil {
-		multi := client.Multi()
-		defer multi.Close()
-		val, err := multi.Watch(key).Result()
+		cs := utils.NewUUID()
+		ts := time.Now().Unix()
+		val := fmt.Sprint(ts, ",", cs)
+		if err := cc.SetNX(key, val, 600*time.Second).Err(); err == nil { //10分钟自动消失
+			// lock exist
+			if old, err := cc.Get(key).Result(); err == nil {
+				vs := strings.SplitN(old, ",", 2)
+				if ots, err := strconv.Atoi(vs[0]); err == nil && int(ts)-ots > 60 { //1分钟过期
+					//过期了,抢
+					if getSet := cc.GetSet(key, val); getSet.Err() == nil {
+						gl := getSet.Val()
+						if gl == old {
+							//抢到了,返回checksum
+							return cs
+						} else {
+							//没抢到,并且还覆盖了人家抢到的锁,(可能会产生问题)
+							//cc.Set(key, gl, 600*time.Second)
+						}
+					}
+				}
+			}
+		}
 	}
 	return ""
+}
+
+/* }}} */
+
+/* {{{ func ReleaseLock(key,cs string) error
+ * 释放锁
+ */
+func ReleaseLock(key, cs string) error {
+	if cc := ClusterClient(); cc != nil {
+		if cur, err := cc.Get(key).Result(); err == nil {
+			vs := strings.SplitN(cur, ",", 2)
+			if vs[1] != cs {
+				Warn("[self: %s][lock: %s][diff]", cs, vs[1])
+			}
+			return cc.Del(key).Err()
+		}
+	}
+	return fmt.Errorf("release wrong")
 }
 
 /* }}} */
